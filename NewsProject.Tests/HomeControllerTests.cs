78ae6a248e0a45e95470aed96e25b4bb95618ec9
Molinarde <lolss1338@gmail.com>
2020-01-20 00:51:09 +0300

    create project

---
+++ NewsProject.Tests/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using NewsProject.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+using Xunit;
+
+namespace NewsProject.Tests
+{
+    class HomeControllerTests
+    {
+        [Fact]
+        public void IndexxViewDataMessage()
+        {
+            //Arrage
+            HomeController controller = new HomeController();
+
+            //act
+            ViewResult result = controller.Index() as ViewResult;
+
+            
+        }
+    }
+}
---
+++ NewsProject/Models/News.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsProject.Models
+{
+    public class News
+    {
+        public int NewsId { get; set; }
+
+        public string Img { get; set; }
+        
+        public string Heading { get; set; }
+        
+        public string ShortDescription { get; set; }
+        
+        public string FullDescription { get; set; }
+    }
+}
